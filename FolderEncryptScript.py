930d386dd0a79575437b8093325ad0414487f722
Robin Reinecke <roboverflow@posteo.de>
2023-01-25 14:50:30 +0100

    optimized folder specification

--- FolderEncryptScript.py
+++ FolderEncryptScript.py
@@ -72,12 +72,7 @@ def decrypt_folder(path, password):
                 # Remove the original encrypted file
                 os.remove(file_path)
 
-
-if len(sys.argv) != 2:
-    print("This script takes only one parameter: Foldername")
-else:
-    # Get folder from arguments
-    folder = sys.argv[1]
+def encrypt_decrypt_folder(folder):
     # Ask the user for the folder password
     password = getpass.getpass("Enter the password: ").encode()
 
@@ -100,3 +95,16 @@ else:
     else:
         encrypt_folder(path, password)
         print("The folder was successfully encrypted.")
+
+if len(sys.argv) == 2:
+    # Get folder from arguments
+    folder = sys.argv[1]
+    encrypt_decrypt_folder(folder)
+elif ((len(sys.argv) < 2) or (sys.argv is None or " ")):
+    # Get folder from user input
+    folder = input("Folder to encrypt: ")
+    encrypt_decrypt_folder(folder)
+elif (len(sys.argv) > 2):
+    print("This script takes only one parameter (folder name).")
+
+
